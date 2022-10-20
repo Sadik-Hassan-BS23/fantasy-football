3295ebf39d8c6cd6a19b21300a8ed050fd5bbc20
Shahinur <shahinur.beg@brainstation-23.com>
2022-10-20 14:54:20 +0600

    User Login Signup Model Created

--- src/main/java/com/brainstation/fantasyfootball/controller/UserController.java
+++ src/main/java/com/brainstation/fantasyfootball/controller/UserController.java
@@ -1,9 +1,32 @@
 package com.brainstation.fantasyfootball.controller;
 
+import com.brainstation.fantasyfootball.request.SignupRequest;
+import com.brainstation.fantasyfootball.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author Shahinur Beg
  * created date: 10/19/2022
  */
+@Controller
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("/signup")
+    public String userSignUp(@RequestBody SignupRequest signupRequest){
+        userService.signup(signupRequest);
+        return null;
+    }
+    @PostMapping("/login")
+    public String userLogin(){
+
+        return  null;
+    }
+
 
 }
--- src/main/java/com/brainstation/fantasyfootball/service/impl/UserServiceImpl.java
+++ src/main/java/com/brainstation/fantasyfootball/service/impl/UserServiceImpl.java
@@ -1,10 +1,30 @@
 package com.brainstation.fantasyfootball.service.impl;
 
+import com.brainstation.fantasyfootball.repository.UserRepository;
+import com.brainstation.fantasyfootball.request.LoginRequest;
+import com.brainstation.fantasyfootball.request.SignupRequest;
 import com.brainstation.fantasyfootball.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Shahinur Beg
  * created date: 10/20/2022
  */
+@Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public ResponseEntity<?> signup(SignupRequest user) {
+
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+        return null;
+    }
 }
