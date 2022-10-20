c2b005684ad641a34bffd0dab4604f2b5a4c882a
Sadik Hassan <sadik.hassan@brainstation-23.com>
2022-10-20 15:22:19 +0600

    Player & Country Updated

--- src/main/java/com/brainstation/fantasyfootball/repository/PlayerRepository.java
+++ src/main/java/com/brainstation/fantasyfootball/repository/PlayerRepository.java
@@ -1,7 +1,14 @@
-package com.brainstation.fantasyfootball.repository;/*
+package com.brainstation.fantasyfootball.repository;
+/*
  * author: Sadik Hassan
  * created: 12:30 pm, 20/10/2022
  */
 
-public class PlayerRepository {
+import com.brainstation.fantasyfootball.model.entity.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player,Long> {
+
 }
